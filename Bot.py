1c11f7e6621464a1e7943f89ce5c7a6ee3858716
AvtorGarryChpoker <127608904+AvtorGarryChpoker@users.noreply.github.com>
2024-06-30 17:45:22 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,9 +7,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
+
 
 @bot.command()
 async def hello(ctx):
